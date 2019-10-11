8fee0938e155c1f6a79ebfb1b9c11761ae70f6ff
Trey Suiter <trey.suiter@gmail.com>
2019-10-10 21:52:53 -0500

    added javascript to add jounral entry object to array

---
+++ src/scripts/journal.js
@@ -0,0 +1,13 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const journalEntry = {
+    testKey: "blah blah blah"
+}
+
+journalArray = []
+
+journalArray.push(journalEntry)
+
+console.log(journalArray)
